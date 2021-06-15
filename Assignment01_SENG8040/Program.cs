5c2ab45d45fd164427cfb703540e9bff06927110
chaudharykbc <kamalbchaudhary@yahoo.com>
2021-06-15 18:40:36 -0400

    Initial Solution file commit

---
+++ Assignment01_SENG8040/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment01_SENG8040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
